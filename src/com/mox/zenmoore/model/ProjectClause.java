2bca2dfdaa3284917b716bd8fc1f6014379792ba
ZenMoore <1057398161@qq.com>
2018-08-17 22:41:56 +0800

    Project

--- src/com/mox/zenmoore/model/ProjectClause.java
+++ src/com/mox/zenmoore/model/ProjectClause.java
@@ -1,7 +1,15 @@
 package com.mox.zenmoore.model;
 
+import javafx.scene.image.Image;
+
 public class ProjectClause extends Model{
 
+    private Image background;
+    private String name;
+    private int level;
+    private String digest;
+    private String startDate;
+    private String endDate;
 
 
     @Override
---
+++ src/com/mox/zenmoore/view/right/Card.java
@@ -0,0 +1,16 @@
+package com.mox.zenmoore.view.right;
+
+import com.mox.zenmoore.model.ProjectClause;
+
+public class Card {
+
+    private ProjectClause projectClause;
+
+    public Card(ProjectClause projectClause) {
+        this.projectClause = projectClause;
+    }
+
+    public ProjectClause getProjectClause() {
+        return projectClause;
+    }
+}
